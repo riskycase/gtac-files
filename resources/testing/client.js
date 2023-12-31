14b3f23762b7832338322e82c94c3491d8e59273
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2023-12-31 13:14:15 +0530

    Some basic stuff to get started
    Signed-off-by: Hrishikesh Patil <hrishikeshpatil.754@gmail.com>

--- resources/testing/client.js
+++ resources/testing/client.js
@@ -1,17 +1,61 @@
 "use strict";
 
-addEventHandler("onPedWasted", function (event, ped, attacker, weapon, piece) {
-    if (ped.isType(ELEMENT_PLAYER)) {
-        message(ped.name + " has died.");
-    } else {
-        message("A ped has died.");
+addEventHandler("onKeyUp", function (event, keycode, scancode, mod) {
+    switch (keycode) {
+        case SDLK_g:
+            break;
+        case SDLK_h:
+            if (localPlayer.isInVehicle) {
+                const vehicle = localPlayer.vehicle;
+                if (localPlayer === vehicle.getOccupant(0)) {
+                    vehicle.velocity = [
+                        vehicle.velocity.x,
+                        vehicle.velocity.y,
+                        vehicle.velocity.z + 0.2,
+                    ];
+                }
+            }
+            break;
+        case SDLK_2:
+            if (localPlayer.isInVehicle) {
+                const vehicle = localPlayer.vehicle;
+                if (localPlayer === vehicle.getOccupant(0)) {
+                    vehicle.velocity = [
+                        vehicle.velocity.x * 1.5,
+                        vehicle.velocity.y * 1.5,
+                        vehicle.velocity.z * 1.5,
+                    ];
+                }
+            }
+            break;
+        case SDLK_y:
+            if (localPlayer.isInVehicle) {
+                const vehicle = localPlayer.vehicle;
+                if (localPlayer === vehicle.getOccupant(0)) {
+                    vehicle.fix();
+                }
+            }
+            break;
+        default:
+            break;
     }
 });
 
-addEventHandler("onKeyUp", function (event, keycode, scancode, mod) {
-    if(keycode === SDLK_g) {
-        console.log(`Pressed g`);
-    } else {
-        console.log(`${keycode} ${scancode}`)
+addCommandHandler("v", function (command, text) {
+    if (!Number.isNaN(Number.parseInt(text))) {
+        createVehicle(Number.parseInt(text), localPlayer);
     }
 });
+
+function createVehicle(id, player) {
+    if (player.isInVehicle) {
+        message("Get out of the current vehicle first!");
+    } else {
+        const vehicle = gta.createVehicle(id, player.position);
+        vehicle.heading = player.heading;
+        vehicle.netFlags.transient = true;
+        vehicle.netFlags.alwaysExistForSyncer = false;
+        addToWorld(vehicle);
+        player.warpIntoVehicle(vehicle, 0);
+    }
+}
--- resources/testing/server.js
+++ resources/testing/server.js
@@ -6,12 +6,12 @@ addCommandHandler("pos", function (command, text, client) {
     );
 });
 
-addCommandHandler("v", function (command, text, client) {
-    if (!Number.isNaN(Number.parseInt(text))) {
-        const vehicle = gta.createVehicle(Number.parseInt(text), client.player.position);
-        addToWorld(vehicle);
-        client.player.warpIntoVehicle(vehicle, 0);
+addEventHandler("onPedWasted", function (event, ped, attacker, weapon, piece) {
+    console.log("Something happened!");
+    if (ped.isType(ELEMENT_PLAYER)) {
+        message(ped.name + " has died.");
+    } else {
+        message("A ped has died.");
     }
 });
-
 
